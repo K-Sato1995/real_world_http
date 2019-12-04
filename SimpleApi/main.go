128f14f58a32013b6b6b34d5106a07abf910de03
Katsuki <Katsuki_Sato0130@icloud.com>
2019-12-04 20:42:07 +0900

    Refactoring

--- SimpleApi/main.go
+++ SimpleApi/main.go
@@ -2,30 +2,30 @@ package main
 
 import (
 	"io/ioutil" // Package ioutil implements some I/O utility functions.
-	"log"
-	"net/http" // Package http provides HTTP client and server implementations.
+	"log"       // Package log implements a simple logging package
+	"net/http"  // Package http provides HTTP client and server implementations.
 )
 
 func main() {
-	log.Println(GetRequest())
+	resp, _ := GetRequest()
+
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	// Log status
+	log.Println(resp.Status)
+	// Log an object of http.Response
+	log.Println(resp)
+	// Converty byte[] to string
+	log.Println(string(body))
 }
 
 // GetRequest makes a simple get requests
-func GetRequest() (string, error) {
+func GetRequest() (*http.Response, error) {
 	resp, err := http.Get("https://k-blog0130.herokuapp.com/api/v2/posts")
 	// Error handling
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
 
-	// Log an object of http.Response
-	log.Println(resp)
-
-	if err != nil {
-		return "", err
-	}
-	// Converty byte[] to string
-	return string(body), nil
+	return resp, nil
 }
