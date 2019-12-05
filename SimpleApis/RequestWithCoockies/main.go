ce76b837ee18fbbc012a3da067ab9bbf19c10990
Katsuki <Katsuki_Sato0130@icloud.com>
2019-12-05 19:37:36 +0900

    Add Request with coockies

---
+++ SimpleApis/RequestWithCoockies/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+	"net/http/httputil"
+)
+
+func main() {
+	// Create a variable that stores a cookie
+	jar, err := cookiejar.New(nil)
+
+	if err != nil {
+		panic(err)
+	}
+	// Create a http client that can store a coockie
+	client := http.Client{
+		Jar: jar,
+	}
+
+	// The client receive a coockie on the first access and send the coockie back to the server from the second access.
+	for i := 0; i < 2; i++ {
+		resp, err := client.Get("http://localhost:3000/coockie")
+		if err != nil {
+			panic(err)
+		}
+		dump, err := httputil.DumpResponse(resp, true)
+
+		if err != nil {
+			panic(err)
+		}
+		log.Println(string(dump))
+	}
+
+}
