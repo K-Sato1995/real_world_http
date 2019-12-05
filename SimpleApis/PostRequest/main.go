be131dcc30a1c5d59054e1e2ef7afecd70c94ebd
Katsuki <Katsuki_Sato0130@icloud.com>
2019-12-05 19:28:20 +0900

    Add post request

---
+++ SimpleApis/PostRequest/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/url"
+)
+
+func main() {
+	values := url.Values{
+		"test": {"value"},
+	}
+	resp, err := http.PostForm("http://localhost:3000", values)
+
+	if err != nil {
+		// Can't make the request
+		log.Println("Error")
+		panic(err)
+	}
+	log.Printf("Status: %v", resp.Status)
+
+}
